47ec79fe9ad48b1dfb1539e3ee9ba2d3fe8796fd
nastinsk <nastinsk@gmail.com>
2019-08-22 16:07:07 -0700

    extra space in schema deleted

--- data/schema.sql
+++ data/schema.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS locations, weather, events;
 
 CREATE TABLE IF NOT EXISTS locations (
 id SERIAL PRIMARY KEY,
-search_query  VARCHAR(255), 
+search_query VARCHAR(255), 
 formatted_query VARCHAR(255), 
 latitude NUMERIC(10,7), 
 longitude NUMERIC(10,7)
